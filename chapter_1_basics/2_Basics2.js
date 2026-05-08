b643b99144114e6892a1b4c7d285183c0dcdaa3d
Shweta Singhal <shwetasinghal23988@gmail.com>
2026-05-08 06:57:40 +0530

    Initial Commit

---
+++ chapter_1_basics/2_Basics2.js
@@ -0,0 +1,11 @@
+let a = 10;
+
+console.log(a);
+
+function print(){
+    console.log("Hello World! This is a function.");
+}
+
+for(let i = 0; i < 10000; i++){
+    print();
+}   
---
+++ chapter_1_basics/3_Basics.js
@@ -0,0 +1,7 @@
+console.log(process.platform);
+
+//to know architecture of the system
+console.log(process.arch);
+
+//to know the node version
+console.log(process.version);
